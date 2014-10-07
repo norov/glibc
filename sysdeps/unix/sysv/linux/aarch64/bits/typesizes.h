363c5174ff98b73f5065222d67e943ed7efd1e21
Andrew Pinski <apinski@cavium.com>
2014-10-07 00:56:11 -0700

    LP64: Move lp64-specific files under corresponding directory
    sysdeps/aarch64/lp64 and sysdeps/unix/sysv/linux/aarch64/lp64
    directories are created, and lp64-specific files moved there
    to avoid incolusion them by aarch64/ilp32 during build.
    
    	* sysdeps/aarch64/Implies: Delete.
    	* sysdeps/aarch64/lp64/Implies: New file.
    	* sysdeps/unix/sysv/linux/aarch64/Implies: Don't mention
    	  unix/sysv/linux/generic and unix/sysv/linux/wordsize-64 (moved
    	  to sysdeps/unix/sysv/linux/aarch64/lp64/Implies)
    	* sysdeps/unix/sysv/linux/aarch64/lp64/Implies: New file.
    	* sysdeps/unix/sysv/linux/aarch64/ioctl.S: Delete (move to
    	  sysdeps/unix/sysv/linux/aarch64/lp64/ioctl.S)
    	* sysdeps/unix/sysv/linux/aarch64/lp64/ioctl.S: New file.
    	* sysdeps/unix/sysv/linux/aarch64/mmap.c: Delete (move to
    	  sysdeps/unix/sysv/linux/aarch64/lp64/mmap.c)
    	* sysdeps/unix/sysv/linux/aarch64/lp64/mmap.c: New file.
    	* sysdeps/unix/sysv/linux/aarch64/shlib-versions: Delete (move to
    	  sysdeps/unix/sysv/linux/aarch64/lp64/shlib-versions)
    	* sysdeps/unix/sysv/linux/aarch64/lp64/shlib-versions: New file.
    
    [AARCH64] ILP32: declare ilp32-specific structures, types and functions
    
    sysdeps/aarch64/ilp32 and sysdeps/unix/sysv/linux/aarch64/ilp32
    directories are created and added to correspoding Implies, to
    host ilp32-specific implementations for:
     - sysdeps/unix/sysv/linux/aarch64/bits/fcntl.h; to enable __O_LARGEFILE
       if __ILP32__ is enabled;
     - sysdeps/unix/sysv/linux/aarch64/bits/stat.c: to set _STAT_VER to
       _STAT_VER_KERNEL, as other generic/wordsize-32 users do, and
       implement non-generic arm32-compatible layout for struct stat;
     - sysdeps/unix/sysv/linux/aarch64/bits/typesizes.h: to set off_t, ino_t
       etc typer to 64-bit to follow modern requirements to 32-bit p0rts;
     - sysdeps/unix/sysv/linux/aarch64/ilp32/kernel_stat.h: to enable
       XSTAT_IS_XSTAT64 and STAT_IS_KERNEL_STAT to let use generic
       implementations for stat syscalls family.
     - sysdeps/unix/sysv/linux/aarch64/ilp32/msgctl.c: to take arm32
       implementation that forces __IPC_64 flag;
     - sysdeps/unix/sysv/linux/aarch64/ilp32/semctl.c: Likewise.
     - sysdeps/unix/sysv/linux/aarch64/ilp32/shmctl.c: Likewise.
     - sysdeps/unix/sysv/linux/aarch64/kernel-features.h: to add
       ilp32-specific features, derived from arm32.
    
    	* sysdeps/unix/sysv/linux/aarch64/bits/fcntl.h: Define
    	  __O_LARGEFILE	to 0400000 if __ILP32__ is enabled.
    	* sysdeps/aarch64/ilp32/Implies: New file.
    	* sysdeps/unix/sysv/linux/aarch64/bits/stat.h: New file.
    	XXX * sysdeps/unix/sysv/linux/aarch64/bits/typesizes.h: New file.
    	* sysdeps/unix/sysv/linux/aarch64/ilp32/Implies: New file.
    	* sysdeps/unix/sysv/linux/aarch64/ilp32/kernel_stat.h: New file.
    	* sysdeps/unix/sysv/linux/aarch64/ilp32/msgctl.c: New file.
    	* sysdeps/unix/sysv/linux/aarch64/ilp32/semctl.c: New file.
    	* sysdeps/unix/sysv/linux/aarch64/ilp32/shlib-versions: New file.
    	* sysdeps/unix/sysv/linux/aarch64/ilp32/shmctl.c: New file.
    	* sysdeps/unix/sysv/linux/aarch64/kernel-features.h: New file.
    
    Signed-off-by: Yury Norov <ynorov@caviumnetworks.com>

--- sysdeps/unix/sysv/linux/aarch64/bits/typesizes.h
+++ sysdeps/unix/sysv/linux/aarch64/bits/typesizes.h
@@ -70,6 +70,15 @@
 /* Same for ino_t and ino64_t.  */
 # define __INO_T_MATCHES_INO64_T			1
 
+/* And for __blkcnt_t and __blkcnt64_t.  */
+# define __BLKCNT_T_TYPE_MATCHES_BLKCNT64_T_TYPE	1
+
+/* And for __fsblkcnt_t and __fsblkcnt64_t.  */
+# define __FSBLKCNT_T_TYPE_MATCHES_FSBLKCNT64_T_TYPE	1
+
+/* And for __fsbilcnt_t and __fsbilcnt64_t.  */
+# define __FSFILCNT_T_TYPE_MATCHES_FSFILCNT64_T_TYPE	1
+
 /* Number of descriptors that can fit in an `fd_set'.  */
 #define	__FD_SETSIZE		1024
 
