pinskia/glibc-ilp32

[AARCH64] Make lp64 and ilp32 directories.
The patch makes the ilp32 and lp64 have their own directory under aarch64.
Since ILP32 uses most of the same system calls as LP64 and has a 64bit
off_t, we need make the functions that end in 64 the same as the ones without.
We also need not to special case ioctl or use the already provided mmap.c file.

ChangeLog:
	* sysdeps/aarch64/ilp32/Implies: New file.
	* sysdeps/aarch64/Implies: Rename to
	* sysdeps/aarch64/lp64/Implies: This.
	* sysdeps/unix/sysv/linux/aarch64/shlib-versions: Rename to ...
	* sysdeps/unix/sysv/linux/aarch64/lp64/shlib-versions: This.
	* sysdeps/unix/sysv/linux/aarch64/ilp32/Implies: New file.
	* sysdeps/unix/sysv/linux/aarch64/ilp32/Versions: New file.
	* sysdeps/unix/sysv/linux/aarch64/ilp32/dl-fxstatat64.c: New file
	* sysdeps/unix/sysv/linux/aarch64/ilp32/fallocate.c: New file.
	* sysdeps/unix/sysv/linux/aarch64/ilp32/fallocate64.c: New file.
	* sysdeps/unix/sysv/linux/aarch64/ilp32/fstatfs64.c: New file.
	* sysdeps/unix/sysv/linux/aarch64/ilp32/ftruncate64.c: New file.
	* sysdeps/unix/sysv/linux/aarch64/ilp32/fxstat.c: New file.
	* sysdeps/unix/sysv/linux/aarch64/ilp32/fxstat64.c: New file.
	* sysdeps/unix/sysv/linux/aarch64/ilp32/fxstatat.c: New file.
	* sysdeps/unix/sysv/linux/aarch64/ilp32/fxstatat64.c: New file.
	* sysdeps/unix/sysv/linux/aarch64/ilp32/getdents.c: New file.
	* sysdeps/unix/sysv/linux/aarch64/ilp32/getdents64.c: New file.
	* sysdeps/unix/sysv/linux/aarch64/ilp32/getrlimit64.c: New file.
	* sysdeps/unix/sysv/linux/aarch64/ilp32/llseek.c: New file.
	* sysdeps/unix/sysv/linux/aarch64/ilp32/lxstat64.c: New file.
	* sysdeps/unix/sysv/linux/aarch64/ilp32/mmap64.c: New file.
	* sysdeps/unix/sysv/linux/aarch64/ilp32/posix_fadvise64.c: New file.
	* sysdeps/unix/sysv/linux/aarch64/ilp32/posix_fallocate.c: New file.
	* sysdeps/unix/sysv/linux/aarch64/ilp32/posix_fallocate64.c: New file.
	* sysdeps/unix/sysv/linux/aarch64/ilp32/pread64.c: New file.
	* sysdeps/unix/sysv/linux/aarch64/ilp3

sysdeps/unix/sysv/linux/aarch64/ilp32/dl-fxstatat64.c
@@ -1,0 +1,6 @@
+/* In this implementation we do not really care whether the call fails
+   because of missing kernel support since we do not even call the
+   function in this case.  */
+#undef __ASSUME_ATFCTS
+#define __ASSUME_ATFCTS 1
+#include "fxstatat.c"

norov/glibc

sysdeps/unix/sysv/linux/aarch64/ilp32/dl-fxstatat64.c
@@ -1,0 +1,6 @@
+/* In this implementation we do not really care whether the call fails
+   because of missing kernel support since we do not even call the
+   function in this case.  */
+#undef __ASSUME_ATFCTS
+#define __ASSUME_ATFCTS 1
+#include "fxstatat64.c"