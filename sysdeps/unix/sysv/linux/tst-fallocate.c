b2d4341dc996aa5163ad0710a15d225d5794dae0
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2016-06-28 16:04:13 -0300

    Consolidate fallocate{64} implementations
    Change from previous version:
    
     - Remove __WORDSIZE and __ASSUME_OFF_DIFF_OFF64 usage.
    
    --
    
    This patch consolidates all the fallocate{64} implementation for Linux
    in only one (sysdeps/unix/sysv/linux/fallocate{64}.c).  It also removes the
    syscall from the auto-generation using assembly macros.
    
    The new macro SYSCALL_LL{64} is used to handle the offset argument.
    
    Checked on x86_64, x32, i386, aarch64, and ppc64le.
    
    	* sysdeps/unix/sysv/linux/fallocate.c (fallocate): Use SYSCALL_LL
    	macro on offset argument.
    	* sysdeps/unix/sysv/linux/fallocate64.c (fallocate64): Use
    	SYSCALL_LL64 on offset argument.
    	* sysdeps/unix/sysv/linux/mips/mips64/n32/fallocate.c: Remove file.
    	* sysdeps/unix/sysv/linux/mips/mips64/n32/fallocate64.c: Likewise.
    	* sysdeps/unix/sysv/linux/mips/mips64/n64/fallocate.c: Likewise.
    	* sysdeps/unix/sysv/linux/mips/mips64/n64/fallocate64.c: Likewise.
    	* sysdeps/unix/sysv/linux/wordsize-64/fallocate.c: Likewise.
    	* sysdeps/unix/sysv/linux/wordsize-64/fallocate64.c: Likewise.
    	* sysdeps/unix/sysv/linux/x86_64/x32/syscalls.list (fallocate): Remove
    	syscall auto-generation.
    	* sysdeps/unix/sysv/linux/Makefile (test): Add tst-fallocate and
    	tst-fallocate64.
    	* sysdeps/unix/sysv/linux/tst-fallocate.c: New file.
    	* sysdeps/unix/sysv/linux/tst-fallocate64.c: Likewise.

---
+++ sysdeps/unix/sysv/linux/tst-fallocate.c
@@ -0,0 +1,117 @@
+/* Basic fallocate test (no specific flags is checked).
+   Copyright (C) 2016 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+/* Allow testing of the 64-bit versions as well.  */
+#ifndef FALLOCATE
+# define FALLOCATE fallocate
+#endif
+
+static void do_prepare (void);
+static int do_test (void);
+#define PREPARE(argc, argv)     do_prepare ()
+#define TEST_FUNCTION           do_test ()
+#include "../test-skeleton.c"
+
+static char *temp_filename;
+static int temp_fd;
+
+void
+do_prepare (void)
+{
+  temp_fd = create_temp_file ("tst-fallocate.", &temp_filename);
+  if (temp_fd == -1)
+    {
+      printf ("cannot create temporary file: %m\n");
+      exit (1);
+    }
+}
+
+#define FAIL(str) \
+  do { printf ("error: %s (line %d)\n", str, __LINE__); return 1; } while (0)
+
+int
+do_test (void)
+{
+  int ret;
+  struct stat finfo;
+  char buffer[1024] = { 0xf0 };
+  char buf2read[1024];
+
+  /* It tries to fallocate 1024 bytes from offset 0 and then write 1024 bytes.
+     After both operation rewind the file descriptor and read 1024 bytes
+     and check if both buffer have the same contents.  */
+  ret = FALLOCATE (temp_fd, 0, 0, 1024);
+  if (ret == -1)
+    FAIL ("fallocate failed");
+
+  ret = fstat (temp_fd, &finfo);
+  if (ret == -1)
+    FAIL ("fstat failed");
+
+  if (finfo.st_size < 1024)
+    FAIL ("size of first fallocate less than expected (1024)");
+
+  if (lseek (temp_fd, 0, SEEK_SET) == (off_t) -1)
+    FAIL ("fseek (0, SEEK_SET) failed");
+
+  if (write (temp_fd, buffer, 1024) != 1024)
+    FAIL ("fail trying to write 1024 bytes");
+
+  if (lseek (temp_fd, 0, SEEK_SET) == (off_t) -1)
+    FAIL ("fseek (0, SEEK_SET) failed");
+
+  if (read (temp_fd, buf2read, 1024) != 1024)
+    FAIL ("fail trying to read 1024 bytes");
+
+  if (memcmp (buffer, buf2read, 1024) != 0)
+    FAIL ("buffer writted different than buffer readed");
+
+  /* It issues the same test as before but using a 1024 bytes offset.  */
+  ret = FALLOCATE (temp_fd, 0, 1024, 2048);
+  if (ret == -1)
+    FAIL ("fallocate failed");
+
+  ret = fstat (temp_fd, &finfo);
+  if (ret == -1)
+    FAIL ("fstat failed");
+
+  if (finfo.st_size < (1024 + 2048))
+    FAIL ("size of second fallocate less than expected (1024 + 2048)");
+
+  if (lseek (temp_fd, 2048, SEEK_SET) == (off_t) -1)
+    FAIL ("fseek (2048, SEEK_SET) failed");
+
+  if (write (temp_fd, buffer, 1024) != 1024)
+    FAIL ("fail trying to write 1024 bytes");
+
+  if (lseek (temp_fd, 2048, SEEK_SET) == (off_t) -1)
+    FAIL ("fseek (2048, SEEK_SET) failed");
+
+  if (read (temp_fd, buf2read, 1024) != 1024)
+    FAIL ("fail trying to read 1024 bytes");
+
+  if (memcmp (buffer, buf2read, 1024) != 0)
+    FAIL ("buffer writted different than buffer readed");
+
+  return 0;
+}
