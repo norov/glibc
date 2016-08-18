54f55bddbfaf0ffb11f7f123e5d5715a1c8ca32f
Yury Norov <ynorov@caviumnetworks.com>
2016-08-18 15:17:05 +0300

    all: #define wordsize-related macros everywhere
    __WORDSIZE_TIME64_COMPAT32 and __WORDSIZE32_SIZE_ULONG should be defined to 0
    but it's not defined at all for many targets.
    
    __WORDSIZE32_PTRDIFF_LONG is introduced for aarch64/ilp32, and defined
    for all other ports as well.
    
    This patch is the part of aarch64/ilp32 work but it can be applied
    separately as it fixes inconsistency in GLIBC.
    
    v2:
      - added __WORDSIZE_TIME64_COMPAT32;
      - added documentation;
      - removed newlines at the end of files.
    v3:
      - __WORDSIZE32_* are meaningless with __WORDSIZE == 64 and left undefined
        in that case.
    v4:
      - fixed typos, formatting.
    
    	* sysdeps/aarch64/bits/wordsize.h : New file
    	(__WORDSIZE32_SIZE_ULONG): Define.
    	(__WORDSIZE32_PTRDIFF_LONG): New define.
    	* sysdeps/mips/bits/wordsize.h: Likewise.
    	* sysdeps/powerpc/powerpc32/bits/wordsize.h: Likewise.
    	* sysdeps/powerpc/powerpc64/bits/wordsize.h: Likewise.
    	* sysdeps/s390/s390-32/bits/wordsize.h: Likewise.
    	* sysdeps/s390/s390-64/bits/wordsize.h: Likewise.
    	* sysdeps/sparc/sparc32/bits/wordsize.h: Likewise.
    	* sysdeps/sparc/sparc64/bits/wordsize.h: Likewise.
    	* sysdeps/tile/tilegx/bits/wordsize.h: Likewise.
    	* sysdeps/tile/tilepro/bits/wordsize.h: Likewise.
    	* sysdeps/unix/sysv/linux/alpha/bits/wordsize.h: Likewise.
    	* sysdeps/unix/sysv/linux/powerpc/bits/wordsize.h: Likewise.
    	* sysdeps/unix/sysv/linux/sparc/bits/wordsize.h: Likewise.
    	* sysdeps/wordsize-32/bits/wordsize.h: Likewise.
    	* sysdeps/wordsize-64/bits/wordsize.h: Likewise.
    	* sysdeps/x86/bits/wordsize.h: Likewise.
    	* sysdeps/generic/stdint.h: Use #if instead of #ifdef for that macros.
    	* sysdeps/gnu/bits/utmp.h: Likewise.
    	* sysdeps/gnu/bits/utmpx.h: Likewise.
    	* bits/wordsize.h: Add documentation.
    
    Signed-off-by: Andrew Pinski <apinski@cavium.com>
    Signed-off-by: Yury Norov <ynorov@caviumnetworks.com>

--- bits/wordsize.h
+++ bits/wordsize.h
@@ -1,1 +1,18 @@
 #error "This file must be written based on the data type sizes of the target"
+
+/* Bits per word (size of CPU register.  */
+#define __WORDSIZE
+
+/* Set to 1 if port is working in 32-bit mode
+   on 64-bit CPU to define SIZE_MAX correctly.
+   Keep undefined for 64-bit ports.  */
+#define __WORDSIZE32_SIZE_ULONG
+
+/* Likewise for PTRDIFF_MIN and PTRDIFF_MAX.  */
+#define __WORDSIZE32_PTRDIFF_LONG
+
+/* Set to 1 to force time types to be 32-bit
+   in struct lastlog and struct utmp{,x}
+   if your 64-bit port should be compatible to
+   32-bit old ports.  */
+#define __WORDSIZE_TIME64_COMPAT32
--- sysdeps/s390/s390-64/bits/wordsize.h
+++ sysdeps/s390/s390-64/bits/wordsize.h
@@ -4,8 +4,11 @@
 # define __WORDSIZE	64
 #else
 # define __WORDSIZE	32
-# define __WORDSIZE32_SIZE_ULONG       1
+# define __WORDSIZE32_SIZE_ULONG	1
+# define __WORDSIZE32_PTRDIFF_LONG	0
 #endif
+
+#define __WORDSIZE_TIME64_COMPAT32	0
 
 #if !defined __NO_LONG_DOUBLE_MATH && !defined __LONG_DOUBLE_MATH_OPTIONAL
 
